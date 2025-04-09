91d8f0d31fc0a7a22c286e6b83ea619de9166684
Isabella Lima <iol1@aluno.ifal.edu.br>
2025-04-09 08:25:09 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+const { soma, multiplicacao } = require('./mathOperations');
+
+const resultadoSoma = soma(10, 5);
+const resultadoMultiplicacao = multiplicacao(10, 5);
+
+console.log(`Resultado da soma: ${resultadoSoma}`); 
+console.log(`Resultado da multiplicação: ${resultadoMultiplicacao}`); 
